df52caab2847ac0e6bb258c7e2357234c0db0341
Jonathan <Jonathan-Schaefer-git>
2023-08-04 20:14:46 +0200

    Changed how received data is proccessed to better align with expectations

--- Scheduling-API/API.fs
+++ Scheduling-API/API.fs
@@ -34,7 +34,7 @@ let apiRoutes =
         POST >=> choose [
             path "/api/solve" >=> 
                 request (fun req -> 
-                        match req.form.[0] |> fst |> jsonToProblem with
+                        match req.rawForm |> Text.Encoding.UTF8.GetString |> jsonToProblem with
                         | Some problem -> 
                             OK (sprintf "%s" (problem |> constructProblem |> JsonConvert.SerializeObject))
                         | None -> BAD_REQUEST "The JSON submitted was found to be invalid"
@@ -48,14 +48,14 @@ let main argv =
     
     // ModularisedScheduling.testCase() |> JsonConvert.SerializeObject |> printfn "%s"
 
-    ModularisedScheduling.testCase() |> constructProblem |> printfn "%A"
+    //ModularisedScheduling.testCase() |> constructProblem |> printfn "%A"
 
-    // let myCfg =
-    //    { Suave.Web.defaultConfig with
-    //        bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 8080 ]
-    //    }
+    let myCfg =
+       { Suave.Web.defaultConfig with
+           bindings = [ HttpBinding.createSimple HTTP "127.0.0.1" 8080 ]
+       }
 
-    // let app = choose [apiRoutes; NOT_FOUND "Route not found. Refer to the source code of the api"]
-    // startWebServer myCfg app
+    let app = choose [apiRoutes; NOT_FOUND "Route not found. Refer to the source code of the api"]
+    startWebServer myCfg app
 
     0 // Return an integer exit code
